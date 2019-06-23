317c6470657d5c63c2ac7223eae6ca912b7cb31f
Akhil Varma Alluri <akhilvarma01@gmail.com>
2019-06-23 18:38:32 -0500

    Update twosum.js

--- Solutions/Two Sum/twosum.js
+++ Solutions/Two Sum/twosum.js
@@ -1,1 +1,11 @@
-test
+const twoSum = function(nums, target) {
+  let a = [];
+  for(let i in nums){
+    const j = target - nums[i];
+    if(typeof(hash[j] !== 'undefined'){
+       return [hash[j],i];
+       }
+      hash[nums[i]] = i;
+      }
+     return [];
+ };
